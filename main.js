59ab7620b74b881e3c3eee4aaaa0d2c93043e0de
“SalahAldeen08” <salahaldeen664@gmail.com>
2023-08-20 11:33:01 +0300

    .

---
+++ main.js
@@ -0,0 +1,36 @@
+// Examples
+console.log(100_000); // 100000
+console.log(100000); // 100000
+console.log(5e4 + 5e4); // 100000
+
+// Your Solutions
+console.log(10**5); // 100000
+console.log(100*1000); // 100000
+console.log(1_000_000/10); // 100000
+console.log(100_000); // 100000
+console.log(100000); // 100000
+console.log(1e5); // 100000
+console.log(5e4 + 5e4); // 100000
+console.log(50000 + 50000); // 100000
+console.log(200000 - 100000); // 100000
+console.log(Math.pow(10, 6)); // 100000
+
+
+console.log(-Number.MIN_SAFE_INTEGER); // 9007199254740991
+
+console.log(Number.MAX_SAFE_INTEGER / Number.MAX_SAFE_INTEGER + Number.MAX_SAFE_INTEGER / Number.MAX_SAFE_INTEGER + Number.MAX_SAFE_INTEGER / Number.MAX_SAFE_INTEGER + Number.MAX_SAFE_INTEGER / Number.MAX_SAFE_INTEGER + Number.MAX_SAFE_INTEGER / Number.MAX_SAFE_INTEGER + Number.MAX_SAFE_INTEGER / Number.MAX_SAFE_INTEGER + Number.MAX_SAFE_INTEGER / Number.MAX_SAFE_INTEGER + Number.MAX_SAFE_INTEGER / Number.MAX_SAFE_INTEGER + Number.MAX_SAFE_INTEGER / Number.MAX_SAFE_INTEGER + Number.MAX_SAFE_INTEGER / Number.MAX_SAFE_INTEGER + Number.MAX_SAFE_INTEGER / Number.MAX_SAFE_INTEGER + Number.MAX_SAFE_INTEGER / Number.MAX_SAFE_INTEGER + Number.MAX_SAFE_INTEGER / Number.MAX_SAFE_INTEGER + Number.MAX_SAFE_INTEGER / Number.MAX_SAFE_INTEGER + Number.MAX_SAFE_INTEGER / Number.MAX_SAFE_INTEGER + Number.MAX_SAFE_INTEGER / Number.MAX_SAFE_INTEGER ); // 16
+
+
+
+
+let myVar = "100.56789 Views";
+
+console.log(parseInt(myVar)) // 100
+console.log(+parseFloat(myVar).toFixed(2)); // 100.57
+
+
+
+
+// let num = 10;
+
+// console.log(10 / (+Number.isInteger(num) + +Number.isInteger(num) + +Number.isInteger(num) + +Number.isInteger(num) + +Number.isInteger(num) )); // 2
